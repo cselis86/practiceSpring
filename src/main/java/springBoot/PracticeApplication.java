9ac94fe9f6ac16aa38285a5d820f7c5191930583
elis <cs_elis@live.com>
2019-03-20 17:05:29 +0200

    start spring Component Scan

--- src/main/java/springBoot/PracticeApplication.java
+++ src/main/java/springBoot/PracticeApplication.java
@@ -1,11 +1,13 @@
-package com.elis.spring.practice;
+package springBoot;
 
 import com.elis.spring.practice.controllers.GreetingController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.elis.spring.practice")
 public class PracticeApplication {
 
     public static void main(String[] args) {
